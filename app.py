a661416b99e8aeb5972ab28329c42affe8576581
khalidmamoun11-crypto <khalidmamoun11@gmail.com>
2026-08-13 13:05:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -504,16 +504,5 @@ init_db()
 print("✅ All done! Starting Flask server...")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
-    import os
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
-    import os
-
-if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
